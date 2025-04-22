f043245efdb04e62989fc2391f174bb915733fe3
Mitch Phillipson <mitch.phillipson@gmail.com>
2025-04-22 08:36:12 -0500

    Initial Commit

---
+++ household.jl
@@ -0,0 +1,88 @@
+using windc_household_model
+
+using DataFrames, MPSGE, PlotlyJS
+
+
+data = load_data(2017)
+
+household = household_model(data);
+
+solve!(household, cumulative_iteration_limit=0)
+
+#zero_profit(household[:KS])
+
+# Set all tariffs to 20%
+set_value!.(household[:tm], .2)
+
+solve!(household)
+
+
+
+
+# This could be automated, but needs work to do
+vars = Dict(
+    :PC => Dict(:domain => (:region, :household)),
+    :PLS => Dict(:domain => (:region, :household)),
+    :C => Dict(:domain => (:region, :household)),
+    :RA => Dict(:domain => (:region, :household)),
+    :LS => Dict(:domain => (:region, :household)),
+    :PD => Dict(:domain => (:region, :good)),
+    :RK => Dict(:domain => (:region, :good)),
+    :PY => Dict(:domain => (:region, :good)),
+    :X => Dict(:domain => (:region, :good)),
+    :A => Dict(:domain => (:region, :good)),
+    :PA => Dict(:domain => (:region, :good)),
+    :Y => Dict(:domain => (:region, :good)),
+    :PM => Dict(:domain => (:region, :margin)),
+    :MS => Dict(:domain => (:region, :margin)),
+    :PN => Dict(:domain => (:good,)),
+    :PL => Dict(:domain => (:region,)),
+    :RKS => Dict(:domain => ()),
+    :NYSE => Dict(:domain => ()),
+    :GOVT_DEMAND => Dict(:domain => ()),
+    :GOVT => Dict(:domain => ()),
+    :PK => Dict(:domain => ()),
+    :KS => Dict(:domain => ()),
+    :INVEST_COMMODITY => Dict(:domain => ()),
+    :GOVT_COMMODITY => Dict(:domain => ()),
+    :PFX => Dict(:domain => ()),
+    :INVEST_DEMAND => Dict(:domain => ()),
+    :INVEST => Dict(:domain => ()),
+)
+function var_to_df(M::MPSGEModel, var::Symbol; value = :value, var_dict = vars)
+    variable_to_dataframe(M[var], var_dict[var][:domain]...; value_name = value)
+end
+
+
+
+
+## Overall Welfare
+description(household[:PC])
+description(household[:PL])
+
+#(PL/PC-1)*100
+
+PC = var_to_df(household, :PC)
+PL = var_to_df(household, :PL)
+
+innerjoin(
+        PC,
+        PL,
+        on = :region,
+        renamecols = "" => "_wage"
+    ) |>
+    x -> transform(x,
+        [:value, :value_wage] => ByRow((pc,pl) -> (pl/pc-1)*100) => :real_value
+    ) |>
+    x -> sort(x, :real_value) |>
+    X -> plot(
+        X, 
+        x = :region, 
+        y = :real_value, 
+        color = :household, 
+        type = :bar, 
+        Layout(
+            title = "Real wages",
+            #yaxis_range = [bounds[1,:min], bounds[1,:max]])
+        )
+    )
---
+++ markusen.jl
@@ -0,0 +1,161 @@
+using markusen_2_2
+
+using MPSGE, JuMP, PATHSolver, DataFrames
+
+
+
+mpsge = mpsge_2_2_taxes()
+
+solve!(mpsge, cumulative_iteration_limit=0)
+
+mcp = mcp_2_2_taxes()
+set_attribute(mcp, "cumulative_iteration_limit", 0)
+optimize!(mcp)
+set_attribute(mcp, "cumulative_iteration_limit", 10_000)
+
+
+
+## Counterfactual 1 - tax_pl = .1
+set_parameter_value(mcp[:tax_pl], 0.1)
+optimize!(mcp)
+
+set_value!(mpsge[:tax_pl], 0.1)
+solve!(mpsge)
+
+vars = [:X, :Y, :W, :PX, :PY, :PW, :PL, :PK, :CONS]
+
+out = "| Variable | MPSGE | MCP |\n| --------- | ----- | --- |\n"
+for var in vars
+    out *= "| $(var) | $(round(value(mpsge[var]),digits=6)) | $(round(value(mcp[var]), digits=6)) |\n"
+end
+
+print(out)
+
+cost_function(mpsge[:X])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function mpsge_2_2_taxes_new()
+
+    M = MPSGEModel()
+
+    @parameters(M, begin
+        tax_pl, 0, (description = "Tax on labor in sector X")
+        tax_pk, 0, (description = "Tax on capital in sector X")
+        sigma, .5, (description = "Elasticity of substitution in sector X")
+    end)
+
+    @sectors(M, begin
+        X
+        Y
+        W
+    end)
+
+    @commodities(M, begin
+        PX
+        PY
+        PW
+        PK
+        PL
+    end)
+
+    @consumer(M, CONS)
+
+    @production(M, X, [t=0, s=sigma, va=>s=1], begin
+        @output(PX, 120, t)
+        @input(PY, 20, s)
+        @input(PL, 40, va, taxes=[Tax(CONS,tax_pl)])
+        @input(PK, 60, va, taxes=[Tax(CONS,tax_pk)])
+    end)
+
+    @production(M, Y, [t=0, s=.75, va=>s=1], begin
+        @output(PY, 120, t)
+        @input(PX, 20, s)
+        @input(PL, 60, va)
+        @input(PK, 40, va)
+    end)
+
+    @production(M, W, [t=0, s=1], begin
+        @output(PW, 200, t)
+        @input(PX, 100, s)
+        @input(PY, 100, s)
+    end)
+
+    @demand(M, CONS, begin
+        @final_demand(PW, 200)
+        @endowment(PL, 100)
+        @endowment(PK, 100)
+    end)
+
+    fix(PW, 1)
+
+    return M
+end
+
+M = mpsge_2_2_taxes_new()
+
+solve!(M, cumulative_iteration_limit=0)
+
+
+set_value!(M[:tax_pl], 0.1)
+
+solve!(M)
+
+df = generate_report(M) |>
+    x -> transform(x,
+        :var => ByRow(y->1) => :counterfactual
+    )
+
+set_value!(M[:sigma], 1)
+
+solve!(M)
+
+df = vcat(df, 
+    generate_report(M) |>
+        x -> transform(x,
+            :var => ByRow(y->2) => :counterfactual
+        )
+)
+
+
+
+set_value!(M[:sigma], 3)
+
+solve!(M)
+
+df = vcat(df, 
+    generate_report(M) |>
+        x -> transform(x,
+            :var => ByRow(y->3) => :counterfactual
+        )
+)
+
+set_value!(M[:tax_pk], .5)
+solve!(M)
+
+df = vcat(df, 
+    generate_report(M) |>
+        x -> transform(x,
+            :var => ByRow(y->4) => :counterfactual
+        )
+)
+
+
+
+df |> 
+    x -> select(x, Not(:margin)) |>
+    x -> unstack(x, :counterfactual, :value)
+
+
